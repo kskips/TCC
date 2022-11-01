1e533b8a2d09d4b499ba00e2e28c8e083e48ed5a
kskips <56672792+kskips@users.noreply.github.com>
2022-11-01 16:49:35 +0000

    Add files via upload

---
+++ inc/clusters/8O.h
@@ -0,0 +1,9 @@
+
+#ifndef TCC_8O_H
+#define TCC_8O_H
+
+void Clusters_Get8O();
+int check_unique_8O(const int *old_6A, int final_part1, int final_part2);
+void add_8O(const int *old_6A, int final_part1, int final_part2);
+
+#endif
