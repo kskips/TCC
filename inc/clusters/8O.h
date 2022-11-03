1bf3ae01b1745470184749fc7e0fc687eaba47b5
kskips <56672792+kskips@users.noreply.github.com>
2022-11-03 15:34:05 +0000

    Add files via upload

--- inc/clusters/8O.h
+++ inc/clusters/8O.h
@@ -3,7 +3,8 @@
 #define TCC_8O_H
 
 void Clusters_Get8O();
-int check_unique_8O(const int *old_6A, int final_part1, int final_part2);
-void add_8O(const int *old_6A, int final_part1, int final_part2);
+int check_unique_8O(int **array);
+void add_8O(int **array);
+int overlap_6A_4A_8O(int **array, const int *clust_6A, const int *clust_4A1, const int *clust_4A2);
 
 #endif
